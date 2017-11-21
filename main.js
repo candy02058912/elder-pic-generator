cb49d589cdfd483463ad683d9eb598bf8d5ebdfd
Candy Tsai <candy_tsai@htc.com>
2017-11-21 17:31:53 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+var canvas = document.querySelector('#canvas');
+var ctx = canvas.getContext("2d");
+var imageObj = new Image();
+
+function downloadImage(img) {
+  var a = document.createElement('a');
+  a.href = savedImg;
+  a.download = "output.png";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+imageObj.onload = function() {
+  ctx.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+  ctx.font = "100pt sans-serif";
+  ctx.fillStyle = 'white';
+  ctx.fillText('測試', 100, 100);
+  ctx.strokeStyle = 'black';
+  ctx.strokeText('測試', 100, 100);
+  var savedImg = canvas.toDataURL();
+
+};
+
+imageObj.src = "test.jpg";
