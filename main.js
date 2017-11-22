2ba4cb010fec60f05e813e267b5c931dc30768b3
Candy Tsai <candy_tsai@htc.com>
2017-11-22 16:06:04 +0800

    add drag and drop box

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var canvas = document.querySelector('#canvas');
 var ctx = canvas.getContext("2d");
 var imageObj = new Image();
+var maxWidth = 500;
 
 function downloadImage(img) {
   var a = document.createElement('a');
@@ -13,8 +14,14 @@ function downloadImage(img) {
 
 function drawImage(text) {
   imageObj.onload = function() {
-    canvas.width = imageObj.width;
-    canvas.height = imageObj.height;
+    imgWidth = imageObj.width;
+    imgHeight = imageObj.height;
+    if (imgWidth > maxWidth) {
+      shrinkRatio = maxWidth / imgWidth;
+    }
+    canvas.width = imgWidth * shrinkRatio;
+    canvas.height = imgHeight * shrinkRatio;
+    
     ctx.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
     ctx.font = "80pt sans-serif";
     ctx.lineWidth = 3;
@@ -27,15 +34,55 @@ function drawImage(text) {
 }
 
 
-document.querySelector('#bottom-text').addEventListener('keyup', function() {
-  console.log(this.value);
+document.querySelector('#bottom-text').addEventListener('input', function() {
   drawImage(this.value);
 });
 
 drawImage('');
 
+var imageLoader = document.getElementById('image-file');
+imageLoader.addEventListener('change', handleImage, false);
 
-var imageFile = document.querySelector('#image-file');
-imageFile.addEventListener('change', function() {
-  console.log('image dropped');
-});
+function handleImage(e) {
+var reader = new FileReader();
+reader.onload = function (event) {
+    var img = event.target.result;
+}
+reader.readAsDataURL(e.target.files[0]);
+
+}
+
+var dropbox = document.querySelector('#drop-box');
+dropbox.addEventListener("dragenter", dragenter, false);
+dropbox.addEventListener("dragover", dragover, false);
+dropbox.addEventListener("dragleave", dragleave, false);
+dropbox.addEventListener("drop", drop, false);
+function dragenter(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  console.log('enter');
+}
+
+function dragleave(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  console.log('leave');
+}
+
+function dragover(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  console.log('over');
+}
+
+function drop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  //you can check e's properties
+  console.log(e);
+  var dt = e.dataTransfer;
+  var files = dt.files;
+  
+  //this code line fires your 'handleImage' function (imageLoader change event)
+  imageLoader.files = files;
+}
